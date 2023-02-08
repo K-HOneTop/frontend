5e0181d918eb7482c7f5ce6c1de2d38891f800c0
Hayeon-Lee <papepopepe@naver.com>
2023-02-09 02:23:51 +0900

    feat: make splash screen

--- src/screens/users/index.tsx
+++ src/screens/users/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
 
 //Users Stack
@@ -11,10 +11,103 @@ export type SplashScreenProps = StackScreenProps<UsersStackParamList, "Splash">;
 //Splash navigation 추가 필요
 const Users = () => {
   return (
-    <View>
-      <Text>이것은 텍스트입니다.</Text>
+    <View style={styles.container}>
+      <View style={styles.topArea}>
+        <View style={styles.logoArea}></View>
+      </View>
+      <View style={styles.midArea}>
+        <View style={styles.intro}>
+          <Text style={styles.introOne}>취뽀의 자격을 시작해보세요</Text>
+          <Text style={styles.introTwo}>취뽀의 자격에서 자격증을</Text>
+          <Text style={styles.introTwo}>
+            뭐시기 저시기 뭐시기 저시기해보세요!
+          </Text>
+        </View>
+      </View>
+      <View style={styles.btmArea}>
+        <TouchableOpacity style={styles.signInBtnBox}>
+          <Text style={styles.signInText}>가입하기</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logInBtnBox}>
+          <Text style={styles.logInText}>로그인하기</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default Users;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topArea: {
+    flex: 0.3,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    //backgroundColor: "green",
+  },
+  logoArea: {
+    flex: 0.3,
+    width: 200,
+    height: 56,
+    backgroundColor: "#D9D9D9",
+  },
+
+  midArea: {
+    flex: 0.9,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    //backgroundColor: "blue",
+  },
+
+  intro: {
+    alignItems: "center",
+  },
+
+  introOne: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  introTwo: {
+    fontSize: 18,
+  },
+
+  btmArea: {
+    flex: 0.4,
+    justifyContent: "center",
+    alignItems: "center",
+    //backgroundColor: "pink",
+  },
+
+  signInBtnBox: {
+    backgroundColor: "#222222",
+    width: 335,
+    height: 52,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+    marginBottom: 3,
+  },
+
+  signInText: {
+    color: "#FFFFFF",
+  },
+
+  logInBtnBox: {
+    backgroundColor: "#868686",
+    width: 335,
+    height: 52,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+    marginTop: 3,
+  },
+
+  logInText: {
+    color: "#FFFFFF",
+  },
+});
--- src/types/stacks/StackTypes.ts
+++ src/types/stacks/StackTypes.ts
@@ -1,3 +1,6 @@
 export type UsersStackParamList = {
   Splash: undefined;
+  Email: string;
+  Nickname: string;
+  Password: string;
 };

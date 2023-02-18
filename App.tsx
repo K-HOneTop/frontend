93cb2613da5a9311a498705e2dddd093584f60db
YujeongLEE <dldbwjd24@ewhain.net>
2023-02-18 21:29:57 +0900

    feat-chore(calendar) : 하단바 스택 작업 이전 단순 확인을 위한 스택

--- App.tsx
+++ App.tsx
@@ -13,57 +13,71 @@ import WelcomeScreen from "./src/screens/users/signUp/Welcome";
 //import home screens
 import HomeScreen from "./src/screens/home/index";
 
+//import calendar screens
+import MainCalendarScreen from './src/screens/calendar/index';
+
 //import types
 import { UsersStackParamList } from "./src/types/stacks/UserStackTypes";
 import { HomeStackParamList } from "./src/types/stacks/HomeStackTypes";
+import { CalendarStackParamList } from "./src/types/stacks/CalendarStackTypes";
 
 //define screen stacks
 const usersStack = createNativeStackNavigator<UsersStackParamList>();
 const homeStack = createNativeStackNavigator<HomeStackParamList>();
+const calendarStack = createNativeStackNavigator<CalendarStackParamList>();
 
 //편의상 Splash로 표기, stack에 다른 화면 type 지정 필요
 const App = () => {
   return (
     <NavigationContainer>
-      <usersStack.Navigator initialRouteName="Splash">
-        <usersStack.Group screenOptions={{ headerShadowVisible: false }}>
-          <usersStack.Screen
-            name="Splash"
-            component={SplashScreen}
-            options={{ headerShown: false }}
+      {/*<usersStack.Navigator initialRouteName="Splash">*/}
+      {/*  <usersStack.Group screenOptions={{ headerShadowVisible: false }}>*/}
+      {/*    <usersStack.Screen*/}
+      {/*      name="Splash"*/}
+      {/*      component={SplashScreen}*/}
+      {/*      options={{ headerShown: false }}*/}
+      {/*    />*/}
+      {/*    <usersStack.Screen*/}
+      {/*      name="AuthCode"*/}
+      {/*      component={AuthCodeScreen}*/}
+      {/*      options={{ title: "", headerShown: true }}*/}
+      {/*    />*/}
+      {/*    <usersStack.Screen*/}
+      {/*      name="Email"*/}
+      {/*      component={EmailScreen}*/}
+      {/*      options={{ title: "", headerShown: true }}*/}
+      {/*    />*/}
+      {/*    <usersStack.Screen*/}
+      {/*      name="UserInfo"*/}
+      {/*      component={UserInfoScreen}*/}
+      {/*      options={{ title: "", headerShown: true }}*/}
+      {/*    />*/}
+      {/*    <usersStack.Screen*/}
+      {/*      name="Login"*/}
+      {/*      component={LoginScreen}*/}
+      {/*      options={{ title: "", headerShown: true }}*/}
+      {/*    />*/}
+      {/*    <usersStack.Screen*/}
+      {/*      name="Welcome"*/}
+      {/*      component={WelcomeScreen}*/}
+      {/*      options={{ headerShown: false }}*/}
+      {/*    />*/}
+      {/*    <homeStack.Screen*/}
+      {/*      name="Home"*/}
+      {/*      component={HomeScreen}*/}
+      {/*      options={{ headerShown: false }}*/}
+      {/*    />*/}
+      {/*  </usersStack.Group>*/}
+      {/*</usersStack.Navigator>*/}
+      <calendarStack.Navigator initialRouteName="Calendar">
+        <calendarStack.Group screenOptions={{ headerShadowVisible: false }}>
+          <calendarStack.Screen
+              name="Calendar"
+              component={MainCalendarScreen}
+              options={{ headerShown: false }}
           />
-          <usersStack.Screen
-            name="AuthCode"
-            component={AuthCodeScreen}
-            options={{ title: "", headerShown: true }}
-          />
-          <usersStack.Screen
-            name="Email"
-            component={EmailScreen}
-            options={{ title: "", headerShown: true }}
-          />
-          <usersStack.Screen
-            name="UserInfo"
-            component={UserInfoScreen}
-            options={{ title: "", headerShown: true }}
-          />
-          <usersStack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ title: "", headerShown: true }}
-          />
-          <usersStack.Screen
-            name="Welcome"
-            component={WelcomeScreen}
-            options={{ headerShown: false }}
-          />
-          <homeStack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{ headerShown: false }}
-          />
-        </usersStack.Group>
-      </usersStack.Navigator>
+        </calendarStack.Group>
+      </calendarStack.Navigator>
     </NavigationContainer>
   );
 };

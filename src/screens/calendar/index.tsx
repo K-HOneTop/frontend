7ec4c0f3bbd156e1b028af3b74ececa7064e6a74
YujeongLEE <dldbwjd24@ewhain.net>
2023-02-18 16:20:45 +0900

    feat(design) : 월/년도 selector 기능 구현

--- src/screens/calendar/index.tsx
+++ src/screens/calendar/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, {useEffect, useState} from "react";
+import {Pressable, View, Text, StyleSheet, TouchableNativeFeedback} from "react-native";
 
 //calendars
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
@@ -8,12 +8,20 @@ import {LocaleConfig} from 'react-native-calendars';
 
 import { StackScreenProps } from "@react-navigation/stack";
 import {CalendarStackParamList} from "../../types/stacks/CalendarStackTypes";
-
+import RNPickerSelect from 'react-native-picker-select';
 
 
 //design 관련
 import styles from './style';
 import {disabledArrowColor, textDayStyle} from "react-native-calendars/src/style";
+import {
+    widthPercentageToDP as wp,
+    heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {getDefaultLocale} from "react-native-calendars/src/services";
+import {isToday} from "react-native-calendars/src/dateutils";
+import {black} from "react-native-paper/lib/typescript/styles/themes/v2/colors";
+
 
 export type MainScreenProps = StackScreenProps<CalendarStackParamList, "Calendar">;
 
@@ -34,9 +42,64 @@ LocaleConfig.defaultLocale = 'fr';
 
  */
 
+type Props = {
+    currentYear: number;
+    currentMonth: number;
+}
+
+
+const CalendarHeader = ({currentYear, currentMonth}:Props) => {
+    const [date, setDate] = useState(new Date())
+    const year = 2023;
+    const month = 1;
+    const [userYear, setUserYear] = useState();
+    const [userMonth, setUserMonth] = useState();
+
+
+    return (
+        <View style = {{flexDirection: 'row'}}>
+            <RNPickerSelect
+                useNativeAndroidPickerStyle={false}
+                style={pickerSelectStyles}
+                onValueChange={(userYear) => setUserYear(userYear)}
+                placeholder={{
+                    label: currentYear,
+                    value: year,
+                    color: 'black',
+                }}
+                items={[
+                    {label: '2021년', value: 2021},
+                    {label: '2022년', value: 2022},
+                    {label: '2023년', value: 2023},
+                ]
+            }
+            />
+            <RNPickerSelect
+                useNativeAndroidPickerStyle={false}
+                style={pickerSelectStyles}
+                onValueChange={(userMonth) => setUserMonth(userMonth)}
+                placeholder={{
+                    label: currentMonth,
+                    value: month,
+                    color: 'black',
+                }}
+                items={[
+                    {label: '1월', value: 2021},
+                    {label: '2월', value: 2022},
+                    {label: '3월', value: 2023},
+                ]
+                }
+            />
+        </View>
+    );
+};
 
 
 const Calendars = ({ navigation }: MainScreenProps) => {
+    const [date, setDate] = useState(new Date());
+
+    console.log(date.getMonth());
+
     return (
         <View style={styles.container}>
             <View style={styles.statusArea}></View>
@@ -48,6 +111,72 @@ const Calendars = ({ navigation }: MainScreenProps) => {
             <View style={styles.mainArea}>
                 <View style={customStyles.mainArea}>
                     <Calendar
+
+                        renderHeader={date => {
+
+
+                            //date :
+                            const dateStr = date.toISOString();
+                            const endIndex = dateStr.indexOf("T");
+                            const yearIndex = dateStr.indexOf("-");
+                            const currentYear = dateStr.slice(0, yearIndex);
+
+                            const monthStr = dateStr.slice(yearIndex+1,endIndex);
+                            const monthIndex = monthStr.indexOf("-");
+                            const currentMonth = monthStr.slice(0, monthIndex);
+
+
+                            const [year, setYear] = useState(currentYear);
+                            const [month, setMonth] = useState(currentMonth);
+
+
+                            // @ts-ignore
+                            return (
+                                <View style={{flex:1, flexDirection: 'row'}}>
+                                    <View style = {{flex:0.5, flexDirection: 'row', alignItems: 'flex-start'}}>
+                                        <RNPickerSelect
+                                            useNativeAndroidPickerStyle={false}
+                                            style={pickerSelectStyles}
+                                            onValueChange={(year) => setYear(year)}
+                                            placeholder={{
+                                                label: year+'년',
+                                                value: year,
+                                                color: 'black',
+                                            }}
+                                            items={[
+                                                {label: '2021년', value: 2021},
+                                                {label: '2022년', value: 2022},
+                                                {label: '2023년', value: 2023},
+                                            ]
+                                            }
+                                        />
+                                        <RNPickerSelect
+                                            useNativeAndroidPickerStyle={false}
+                                            style={pickerSelectStyles}
+                                            onValueChange={(month) => setMonth(month)}
+                                            placeholder={{
+                                                label: month+'월',
+                                                value: month,
+                                                color: 'black',
+                                            }}
+                                            items={[
+                                                {label: '1월', value: 1},
+                                                {label: '2월', value: 2},
+                                                {label: '3월', value: 3},
+                                            ]
+                                            }
+                                        />
+                                </View>
+                                    <View style = {{flex : 0.5, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end'}}>
+                                        <Pressable style={styles.DesignButton}>
+                                            <Text style={customStyles.btnText}>
+                                                {'필터링 버튼'}
+                                            </Text>
+                                        </Pressable>
+                                    </View>
+                                </View>
+                            );
+                        }}
                         theme={{
                             // @ts-expect-error
                             'stylesheet.calendar.header': {
@@ -141,6 +270,36 @@ const customStyles = StyleSheet.create({
         //요소 중앙배치
         // alignItems: 'center',
     },
-    calendar:{
+    btnText: {
+        color: 'white',
+        fontSize: wp(3),
+        justifyContent: 'center',
+        alignItems: 'center',
     }
+
+});
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        // height: hp(4),
+        paddingRight: 3,
+        fontSize: wp(4.5),
+        fontWeight: 'normal',
+        marginBottom: 0,
+    },
+
+    inputAndroid: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        // height: hp(6),
+        paddingRight: 3,
+        fontSize: wp(4.5),
+        fontWeight: 'normal',
+        marginBottom: 0,
+    },
+
 });

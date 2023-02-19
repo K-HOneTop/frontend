bdaaa0c4e73781874a87e1ddc37bc2ba445b4236
YujeongLEE <dldbwjd24@ewhain.net>
2023-02-19 23:31:40 +0900

    feat (star icon) : star icon 추가

---
+++ src/screens/Bookmark/components/StarIcon.tsx
@@ -0,0 +1,44 @@
+import React,{useState} from "react";
+import {Image, Pressable, StyleSheet, View} from "react-native";
+
+type Props = {
+    selected:boolean;
+}
+
+function StarIcon ({selected}:Props) {
+    const [isSelected, setIsSelected] = useState<boolean>(selected);
+
+
+    const onChange = () => {
+        setIsSelected(!isSelected);
+    }
+
+
+    return (
+        <View>
+            <Pressable
+                style={customStyles.star}
+                onPress={onChange}>
+                {isSelected === true ? (
+                    <Image source={require('../../../assets/images/star_on.png')}/>
+                ) : (<Image source={require('../../../assets/images/star_off.png')}/>)
+                }
+            </Pressable>
+        </View>
+    );
+};
+
+
+
+export default StarIcon;
+
+
+const customStyles = StyleSheet.create({
+
+    star:{
+        position: 'absolute',
+        right:11.5,
+        bottom:12,
+    }
+
+});
